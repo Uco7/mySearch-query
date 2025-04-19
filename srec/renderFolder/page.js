3eb12ed640c7a72aa2e987817ecacdea3e882d8b
uche innocent <ucheuc070@gmail.com>
2025-04-20 02:44:47 +1245

    my second comit to this project

--- srec/renderFolder/page.js
+++ srec/renderFolder/page.js
@@ -26,6 +26,142 @@ module.exports={
     postGradute_program:(req,res)=>{
         res.render("./funai/postGradute_program")
     },
+    // unn fieldEnds...........................................
+    unn_course_dept:(req,res)=>{
+        res.render("./UNN/change_course_dept")
+    },
+    uun_fees:(req,res)=>{
+        res.render("./UNN/fees")
+    },
+    unn_jamb:(req,res)=>{
+        res.render("./UNN/jamb_courses_for_program")
+    },
+    unn_reg_courses:(req,res)=>{
+        res.render("./UNN/reg_courses")
+    },
+    unn_admissionReq:(req,res)=>{
+        res.render("./UNN/unn_admissionRequirement")
+    },
+   
+
+    // unn fieldEnds........................................... end///
+    // unilag.........................................................///
+    uni_lag_cut_of_marks:(req,res)=>{
+        res.render("./unniLag/unilag_eng_cut_off_marks")
+    },
+    uniLag:(req,res)=>{
+        res.render("./unniLag/uniLag")
+    },
+    // unilag......................................................... end  ///
+    // OAU  ........................................................................//
+    oau_admissionRequirements:(req,res)=>{
+        res.render("./OAU/oau_law_admission-requirement")
+    },
+    oau:(req,res)=>{
+        res.render("./OAU/oau")
+    }, 
+    // OAU  .........................................................end ...............//
+    // //ebsu...........................................................////////////
+    ebsu_direct_entry_query:(req,res)=>{
+        res.render("./ebsu/ebsu_direct_entry_guide")
+    }, 
+    ebsu:(req,res)=>{
+        res.render("./ebsu/ebsu")
+    }, 
+    ebsu_school_fee:(req,res)=>{
+        res.render("./ebsu/ebsu_schoo_ees")
+    }, 
+    // ebsu........................................................... end ////////////
+
+////////////// UI..................................................................////////////
+ui:(req,res)=>{
+    res.render("./Ui/ui")
+}, 
+ui_school_fees:(req,res)=>{
+    res.render("./Ui/ui_school_fees")
+}, 
+////////////// UI..................................................................end////////////
+////////////////// fresource_folder ........................................../////////////
+cgp_calc:(req,res)=>{
+    res.render("./resourceFolder/cgpaCalc")
+}, 
+schools_direct_entry_query:(req,res)=>{
+    res.render("./resourceFolder/cross_direct_entry")
+}, 
+universities_medcine_and_surgry:(req,res)=>{
+    res.render("./resourceFolder/medcine_surgriy_university")
+}, 
+scholarship_guid:(req,res)=>{
+    res.render("./resourceFolder/scholarship_guide")
+}, 
+school_grading_system_app:(req,res)=>{
+    res.render("./resourceFolder/school_grading_system")
+}, 
+most_demanding_courses:(req,res)=>{
+    res.render("./resourceFolder/most_demandingCourses")
+},
+waec_neco_qae:(req,res)=>{
+    res.render("./resourceFolder/waec_necoQea")
+},
+career:(req,res)=>{
+    res.render("./resourceFolder/career")
+},
+courses:(req,res)=>{
+    res.render("./resourceFolder/courses")
+},
+Ngn_institution_list:(req,res)=>{
+    res.render("./resourceFolder/high_institution")
+},
+
+// jamb .............................................................
+
+jamb_syllabus:(req,res)=>{
+    res.render("./resourceFolder/jamb_brochio_sylable")
+},
+jamb_data_corection:(req,res)=>{
+    res.render("./resourceFolder/jamb_data_corection")
+},
+jamb_reg_date:(req,res)=>{
+    res.render("./resourceFolder/jamb_reg_date")
+},
+jamb_freq_qea:(req,res)=>{
+    res.render("view/resourceFolder/jambFrequentQEA")
+},
+jamb_dead_line:(req,res)=>{
+    res.render("./resourceFolder/jambReg_dead_line")
+},
+jamb_reprinting_date:(req,res)=>{
+    res.render("./resourceFolder/jambReg_reprintingDate")
+},
+jambReg_requirement:(req,res)=>{
+    res.render("./resourceFolder/jambReg_requirement")
+},
+
+
+// jamb .............................................................end///
+
+
+////////////////// fresource_folder ........................................../////////////
+
+// navigation items ..........................................
+privacy_policy:(req,res)=>{
+    res.render("privacy_policy")
+},
+contact:(req,res)=>{
+    res.render("contact")
+},
+about:(req,res)=>{
+    res.render("about")
+},
+disclaimer:(req,res)=>{
+    res.render("disclaimer")
+},
+// navigation items ..........................................
+
+
+
+
+
     home:(req,res)=>{
         res.render("index")
     }
--- srec/routeFolder/getRoute.js
+++ srec/routeFolder/getRoute.js
@@ -14,7 +14,72 @@ getPage_Route.get("/funai-post-gradute-guide-procedures/guide",getPage.postGradu
 // funai get rout end.......................................................................................
 // UNN get rout...............................................................getPage...........................getPage............
 
+getPage_Route.get("/unn-courses/dept",getPage.unn_course_dept)
+getPage_Route.get("/unn-fees",getPage.uun_fees)
+getPage_Route.get("/unn-jamb/courses/programs",getPage.unn_jamb)
+getPage_Route.get("/unn-reg/courses",getPage.unn_reg_courses)
+getPage_Route.get("/unn-admission/requirement",getPage.unn_admissionReq)
+
+
 // UNN get rout  end...............................................................getPage...........................getPage............
+
+// uniLag.......................................................................
+getPage_Route.get("/uni-lag/cut-of-marks",getPage.uni_lag_cut_of_marks)
+getPage_Route.get("/uni-lag",getPage.uniLag)
+// uniLag.......................................................................end///
+// AutoEncryptionLoggerLevel.......................................................
+getPage_Route.get("/aou/admission-requirement",getPage.oau_admissionRequirements)
+getPage_Route.get("/aou",getPage.oau)
+// AutoEncryptionLoggerLevel.......................................................
+
+
+    // //ebsu...........................................................////////////
+    getPage_Route.get("/ebsu_direct_entry/query",getPage.ebsu_direct_entry_query)
+    getPage_Route.get("/ebsu_school/fees",getPage.ebsu_school_fee)
+    getPage_Route.get("/ebsu",getPage.ebsu)
+    
+
+    // //ebsu........................................................... end////////////
+    ///////////Ui........................................................../////////////
+    getPage_Route.get("/ui",getPage.ui)
+    getPage_Route.get("/ui_school_fees/charges",getPage.ui_school_fees)
+
+    /////////////////////Ui...........................end/////////////////////////
+   /////////// resource folder....................................................//
+   getPage_Route.get("/calculate/cgpa/app",getPage.cgp_calc)
+   getPage_Route.get("/school-direct/entry/query",getPage.schools_direct_entry_query)
+   getPage_Route.get("/schools/grading/system",getPage.school_grading_system_app)
+   getPage_Route.get("/universities/medcine/surgry",getPage.universities_medcine_and_surgry)
+   getPage_Route.get("/scholarship/guid",getPage.scholarship_guid)
+   getPage_Route.get("/career/page",getPage.career)
+   getPage_Route.get("/most-demanding/courses",getPage.most_demanding_courses)
+   getPage_Route.get("/waec-neco/freq/questions",getPage.waec_neco_qae)
+   getPage_Route.get("/courses/page",getPage.courses)
+   getPage_Route.get("/jamb/syllabus",getPage.jamb_syllabus)
+   getPage_Route.get("/jamb/data/correction/page",getPage.jamb_data_corection)
+   getPage_Route.get("/jamb-reg-date/page",getPage.jamb_reg_date)
+   getPage_Route.get("/jamb-frequent/asked/question",getPage.jamb_freq_qea)
+   getPage_Route.get("/jamb-reg/deadline/page",getPage.jamb_dead_line)
+   getPage_Route.get("/jamb-reprinting/date",getPage.jamb_reprinting_date)
+   getPage_Route.get("/jamb-reg/requirements/page",getPage.jambReg_requirement)
+   getPage_Route.get("/list-of-institution/ngn/page",getPage.Ngn_institution_list)
+   
+
+  
+   /////////// resource folder...........................................end.........//
+// navigation items ..........................................
+getPage_Route.get("/privacy/policy/page",getPage.privacy_policy)
+getPage_Route.get("/contact-us/page",getPage.contact)
+getPage_Route.get("/disclaimer/page",getPage.disclaimer)
+getPage_Route.get("/about-us/page",getPage.about)
+
+
+// navigation items ..........................................
+
+
+
+
+
 getPage_Route.get("/",getPage.home)
 
 module.exports=getPage_Route 
